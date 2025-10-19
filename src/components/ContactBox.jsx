61130f6fa0015db11ec09842509ab28ff1a77c22
Kebron Gizachew <kebisel21@gmail.com>
2025-10-19 20:49:39 +0300

    a little modification on the media queryof the contact box

--- src/components/ContactBox.jsx
+++ src/components/ContactBox.jsx
@@ -5,11 +5,11 @@ function ContactBox(props) {
   const {theme } = useContext(PropContext);
 
   return (
-    <div className="w-[400px] h-[150px] bg-transparent flex rounded-[30px] border-1 border-[#9f9e9eff] justify-around items-center p-0 max-[768px]:w-full max-[768px]:h-auto max-[768px]:flex-col max-[768px]:p-[20px] max-[768px]:gap-[15px]">
+    <div className="w-[400px] h-[150px] bg-transparent flex rounded-[30px] border-1 border-[#9f9e9eff] justify-around items-center p-0 max-[768px]:w-full max-[768px]:h-auto max-[768px]:flex-col max-[768px]:p-[20px] max-[768px]:gap-[15px] max-[768px]:items-center max-[768px]:justify-center max-[768px]:pl-[-60px]">
       <div className="bg-[#fadfcdff] w-[80px] h-[80px] rounded-[50%] flex justify-center items-center text-[40px] text-[#ff6600] mr-[-50px] max-[768px]:w-[60px] max-[768px]:h-[60px] max-[768px]:text-[30px] max-[768px]:mr-0">{props.icon}</div>
       <div className="p-0 h-full w-[70%] flex-col text-left justify-center max-[768px]:w-full max-[768px]:text-center max-[768px]:items-center ml-[40px]">
         <p className="text-[#908f8fff] text-[30px] font-bold mt-[35px] ml-0 text-left mb-[5px] max-[768px]:text-[22px] max-[768px]:mt-[10px] max-[768px]:mb-[5px] max-[768px]:text-center">{props.main}</p>
-      <p className="text-[25px] m-auto max-[768px]:text-[18px] max-[768px]:mt-0 max-[768px]:text-center" style={{color:theme.color}}>
+      <p className="text-[25px] m-auto max-[768px]:text-[18px] max-[768px]:mt-0 max-[768px]:text-center max-[768px]:m-auto" style={{color:theme.color}}>
           {props.info}
       </p>
       </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,12 +9,12 @@ function Contact() {
   const {theme} = useContext(PropContext)
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen w-screen p-[20px] transition-all duration-300 ease-in-out h-full mt-[0px] max-[768px]:p-10 max-[768px]:min-h-auto max-[768px]:h-auto" style={{backgroundColor:theme.backgroundColor, color:theme.color }}>
-      <Header/>
-      <h1 className="text-[60px] font-bold mb-[20px] mt-[100px] max-[768px]:text-[36px] max-[768px]:mt-[50px] max-[768px]:mb-[15px] max-[768px]:text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen p-[20px] transition-all duration-300 ease-in-out h-full mt-[0px] max-[768px]:min-h-auto max-[768px]:h-auto max-[768px]:p-0 max-[768px]:w-full" style={{backgroundColor:theme.backgroundColor, color:theme.color }}>
+      <Header />
+      <h1 className="text-[60px] font-bold mb-[20px] mt-[100px] max-[768px]:text-[36px] max-[768px]:mt-[50px] max-[768px]:mb-[10px] max-[768px]:text-center">
         Get In <span style={{color:"#FF6600" }}>Touch</span>
       </h1>
-      <div className="w-[150px] bg-[#FF6600] h-[10px] mb-[-40px] max-[768px]:w-[130px] max-[768px]:h-[6px] max-[768px]:mb-[-20px]"></div>
+      <div className="w-[150px] bg-[#FF6600] h-[10px] mb-[-40px] max-[768px]:w-[130px] max-[768px]:h-[6px] max-[768px]:mb-[20px]"></div>
       <div className="flex justify-center items-center flex-wrap w-[80%] gap-[50px] pb-[50px] max-[768px]:flex-col max-[768px]:w-full max-[768px]:gap-[25px] max-[768px]:pb-[30px]">
         <ContactInfo />
         <ContactForm/>
